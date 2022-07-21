4d651ee0ea256d7192831908313cd8b739c5aafe
Mov Seng <mov.seng@mindgeekconsultants.com>
2022-07-21 11:12:36 +0700

    Add list view css

--- src/components/ContactListViewItems.js
+++ src/components/ContactListViewItems.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './SideBar.css';
+import './ContactListViewItems.css';
 import {Link} from "react-router-dom";
 import {updateData} from "../services";
 
@@ -15,34 +16,34 @@ const ContactListViewItems = (props) => {
                         <div className="details">
                             <div className="name">{item.name}</div>
                             <div className="job">{item.company + ', ' + item.position}</div>
+                            <div className="city">{item.city}</div>
+                            <div className="media-icons">
+                                {item.social_networks.facebook?.length > 0 && (
+                                    <Link to={item.social_networks.facebook}><i className="fa fa-facebook-f"></i></Link>
+                                )}
+                                {item.social_networks.twitter?.length > 0 && (
+                                    <Link to={item.social_networks.twitter}><i className="fa fa-twitter"></i></Link>
+                                )}
+                                {item.social_networks.instagram?.length > 0 && (
+                                    <Link to={item.social_networks.instagram}><i className="fa fa-instagram"></i></Link>
+                                )}
+                                {item.social_networks.youtube?.length > 0 && (
+                                    <Link to={item.social_networks.youtube}><i className="fa fa-youtube"></i></Link>
+                                )}
+                            </div>
                         </div>
-                        <div className="media-icons">
-                            {item.social_networks.facebook?.length > 0 && (
-                                <Link to={item.social_networks.facebook}><i className="fa fa-facebook-f"></i></Link>
+                        <div className="list-action">
+                            {item.isContact ? (
+                                <button onClick={() => updateData('contact', 'delete', index)} className="btn btn-danger">Delete from contacts</button>
+                            ):(
+                                <button onClick={() => updateData('contact', 'add', index)} className="btn btn-success">Add to contacts</button>
                             )}
-                            {item.social_networks.twitter?.length > 0 && (
-                                <Link to={item.social_networks.twitter}><i className="fa fa-twitter"></i></Link>
-                            )}
-                            {item.social_networks.instagram?.length > 0 && (
-                                <Link to={item.social_networks.instagram}><i className="fa fa-instagram"></i></Link>
-                            )}
-                            {item.social_networks.youtube?.length > 0 && (
-                                <Link to={item.social_networks.youtube}><i className="fa fa-youtube"></i></Link>
+                            {item.isFavourite ? (
+                                <button onClick={() => updateData('favorite', 'delete', index)} className="btn btn-danger">Delete from favorites</button>
+                            ):(
+                                <button onClick={() => updateData('favorite', 'add', index)} className="btn btn-success">Add to favorites</button>
                             )}
                         </div>
-                        <div className="details">
-                            <div className="job">{ item.city}</div>
-                        </div>
-                        {item.isContact ? (
-                            <button onClick={() => updateData('contact', 'delete', index)} className="btn btn-danger">Delete from contacts</button>
-                        ):(
-                            <button onClick={() => updateData('contact', 'add', index)} className="btn btn-success">Add to contacts</button>
-                            )}
-                        {item.isFavourite ? (
-                            <button onClick={() => updateData('favorite', 'delete', index)} className="btn btn-danger">Delete from favorites</button>
-                        ):(
-                            <button onClick={() => updateData('favorite', 'add', index)} className="btn btn-success">Add to favorites</button>
-                        )}
                     </div>
                 </div>
             )
